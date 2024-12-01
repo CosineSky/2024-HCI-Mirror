9b17698702a306414183de4ab720180de4fa6f0c
daify <221250046@smail.nju.edu.cn>
2024-12-01 20:23:25 +0800

    增加header的最基础实现

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <script setup>
+import {useRouter} from "vue-router";
 
+const router = useRouter()
 </script>
 
 <template>
   <div class="header">
+    <router-link to="/home" class="home-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+        <polyline points="9 22 9 12 15 12 15 22"></polyline>
+      </svg>
+    </router-link>
     <input type="text" placeholder="想播放什么？" />
-    <button class="login-btn">登录</button>
+    <button class="login-btn" >登录</button>
   </div>
 </template>
 
@@ -27,6 +35,20 @@
   -webkit-backdrop-filter: blur(10px);
   box-shadow: 0 4px 8px rgba(200, 187, 255, 0.3);
   border: 1px solid #fff;
+}
+
+.home-btn {
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  color: #fff;
+  text-decoration: none;
+}
+
+.home-btn svg {
+  width: 24px;
+  height: 24px;
+  margin-right: 5px;
 }
 
 input[type="text"] {
